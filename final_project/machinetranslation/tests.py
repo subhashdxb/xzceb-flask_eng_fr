d7d4bf22b23fadbf79eae2aa86667984f6d43ad2
subhashdxb <sreenivasansubhash@gmail.com>
2021-09-05 14:05:03 +0000

    Final changes Sept5

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -4,14 +4,13 @@ from translator import englishToFrench, frenchToEnglish
 class TestSquare(unittest.TestCase): 
     def test1(self):
         """Unit Test for English to French Translation""" 
-        self.assertEqual(englishToFrench(''))
+        self.assertEqual(englishToFrench(''),)
         self.assertEqual(englishToFrench('Hello'), 'Bonjour')
-        self.assertNotEqual(englishToFrench('How Much' 'Bonjour'))
+        self.assertNotEqual(englishToFrench('How Much'), 'Bonjour')
 
 class TestSquare(unittest.TestCase): 
     def test1(self):
         """Unit Test for French to English Translation"""
-        self.assertEqual(frenchToEnglish(''))
         self.assertEqual(frenchToEnglish('Bonjour'), 'Hello')
-        self.assertNotEqual(frenchToEnglish('Bonjour' 'How Much'))
+        self.assertNotEqual(frenchToEnglish('Bonjour'), 'How Much')
 unittest.main()
